5bab3bc89efb8e3ff8ec7d80b772886b1c7ecbd0
JhanME <mocaicojhan@gmail.com>
2025-12-29 17:05:48 -0500

    logIn-integrate-gaa

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,5 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 console.log('Auth route loaded');
-
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
